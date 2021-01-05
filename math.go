556c4e2e2e2c5a1fe6b3ee11363671396b6c3203
Willer César de Freitas <willer.freitas@tqi.com.br>
2021-01-05 09:39:55 -0300

    feat: add sonar properties

--- math.go
+++ math.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-
 	fmt.Println(soma(10, 10))
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
 }
